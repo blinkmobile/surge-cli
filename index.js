9f4bad4d994c51461cc3abe5c3b63b157ede80ab
Ron Waldon <jokeyrhyme@gmail.com>
2015-11-27 12:15:19 +1100

    show useful error if surge is not installed

--- index.js
+++ index.js
@@ -13,4 +13,11 @@ childProcess.spawn('surge', process.argv.slice(2), {
   detached: false,
   env: process.env,
   stdio: 'inherit'
-});
+})
+  .on('error', (err) => {
+    if (err && err.code === 'ENOENT') {
+      console.error(`Error: "surge" executable not found\n`);
+      console.log(`Make sure you have installed surge first: npm -g install surge`);
+      process.exit(1);
+    }
+  });
